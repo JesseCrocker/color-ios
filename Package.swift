95900d55b27d0a0f8ee334b882255f44fad6dec8
Jesse Crocker <jesse@datamongers.net>
2024-12-18 15:08:29 -0700

    add package.swift file

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "color-ios",
+    platforms: [
+        .iOS(.v12),
+        .macCatalyst(.v13)
+    ],
+    products: [
+        .library(
+            name: "color_ios",
+            targets: ["color_ios"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "color_ios",
+            path: "color-ios",
+            exclude: ["Info.plist"],
+            sources: [
+                "CLRColor.h",
+                "CLRColor.m",
+                "CLRColorConstants.h",
+                "CLRColorConstants.m",
+                "CLRColorUtils.h",
+                "CLRColorUtils.m",
+                "color_ios.h",
+                "color_ios.swift"
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        ),
+        .testTarget(
+            name: "color_iosTests",
+            dependencies: ["color_ios"],
+            path: "color-iosTests",
+            exclude: ["Info.plist"]
+        )
+    ]
+)
